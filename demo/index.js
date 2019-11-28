b9baf88795184ecb1c0c526d028dfbbbc36d6461
Matías Martínez <soy@matmartinez.net>
2019-11-28 16:54:50 -0300

    Added initial implementation.

---
+++ demo/index.js
@@ -0,0 +1,27 @@
+import Pimiento from "../Pimiento.js"
+
+const node = new Pimiento.Node()
+node.frame = Pimiento.Rect.make(0, 0, 100, 100)
+node.background = "red"
+node.cornerRadius = 16
+
+const textNode = new Pimiento.TextNode()
+textNode.text = "Hello, world"
+textNode.font = "80px system-ui"
+textNode.frame = Pimiento.Rect.make(0, 0, 1000, 100)
+
+const imageNode = new Pimiento.ImageNode()
+imageNode.src = "https://dummyimage.com/200x200/a312a3/fff.png"
+imageNode.frame = Pimiento.Rect.make(50, 50, 100, 100)
+imageNode.background = "blue"
+
+const scene = new Pimiento.Scene()
+scene.addChild(node)
+scene.addChild(textNode)
+scene.addChild(imageNode)
+
+const renderer = new Pimiento.Renderer()
+renderer.size = Pimiento.Size.make(500, 120)
+renderer.scene = scene
+
+document.body.appendChild(renderer.DOMElement)
---
+++ pimiento.js
@@ -0,0 +1,18 @@
+import Node from "./lib/Node.js"
+import ImageNode from "./lib/ImageNode.js"
+import TextNode from "./lib/TextNode.js"
+import Renderer from "./lib/Renderer.js"
+import Scene from "./lib/Scene.js"
+import { Rect, Size, Point, Insets } from "./lib/Geometry.js"
+
+export default {
+  Node,
+  ImageNode,
+  TextNode,
+  Renderer,
+  Scene,
+  Rect,
+  Size,
+  Point,
+  Insets
+}
